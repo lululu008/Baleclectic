78013d96e221c0bbefe01f1d647588c6c8eb7f51
lin <sining040@gmail.com>
2020-02-29 17:10:17 +0100

    push for LU

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -1,14 +1,28 @@
 package ch.epfl.sdp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "ch.epfl.sdp.EXTRA_MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.mainName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
