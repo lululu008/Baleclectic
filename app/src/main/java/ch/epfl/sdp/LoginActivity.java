c1a1bde8214c2a18668320370e56f4ce1affb47e
JiajunSang <meetjiajun@gmail.com>
2020-03-08 22:30:11 +0100

    new

--- app/src/main/java/ch/epfl/sdp/LoginActivity.java
+++ app/src/main/java/ch/epfl/sdp/LoginActivity.java
@@ -1,11 +1,18 @@
 package ch.epfl.sdp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,34 +38,35 @@ public class LoginActivity extends AppCompatActivity {
                 .build(),RC_SIGN_IN);
     }
 
-//    // [START auth_fui_result]
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == RC_SIGN_IN) {
-//            IdpResponse response = IdpResponse.fromResultIntent(data);
-//
-//            if (resultCode == RESULT_OK) {
-//                // Successfully signed in
-//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(this, ""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-//    // [END auth_fui_result]
-//
-//    public void signOut() {
-//        // [START auth_fui_signout]
-//        AuthUI.getInstance()
-//                .signOut(this)
-//                .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                    public void onComplete(@NonNull Task<Void> task) {
-//                        // ...
-//                    }
-//                });
-//        // [END auth_fui_signout]
-//    }
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+            } else {
+                Toast.makeText(this, ""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    // [END auth_fui_result]
+
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_signout]
+    }
 }
