d953ea0b64ed96a253a844a7547964878686a970
yutong0330 <yutongsong.0330@gmail.com>
2020-03-09 01:40:18 +0100

    modified main activity

--- app/src/main/java/ch/epfl/sdp/ProfileActivity.java
+++ app/src/main/java/ch/epfl/sdp/ProfileActivity.java
@@ -1,8 +1,16 @@
 package ch.epfl.sdp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +18,52 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button sign_out = (Button)findViewById(R.id.sign_out_button);
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(startIntent);
+                signOut();
+            }
+        });
+
+        Button delete_account = (Button)findViewById(R.id.delete_account_button);
+        delete_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(startIntent);
+                deleteAccount();
+            }
+        });
+
     }
+
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+
+    public void deleteAccount() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_delete]
+    }
+
 }
