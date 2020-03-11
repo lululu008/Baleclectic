8031d4c7bbd70335befe6cf632ce329bb61f90d2
lxy <lu852001630@163.com>
2020-03-11 18:14:56 +0100

    test: Swipe on top instead of center

--- app/src/androidTest/java/ch/epfl/sdp/TimeTableTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/TimeTableTest.java
@@ -1,9 +1,9 @@
 package ch.epfl.sdp;
 
-import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.CoordinatesProvider;
 import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
@@ -18,19 +18,17 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class TimeTableTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
 
     private static ViewAction swipeFromRightToLeft() {
-        return new GeneralSwipeAction(Swipe.SLOW, GeneralLocation.CENTER_RIGHT,
-                GeneralLocation.CENTER_LEFT, Press.FINGER);
+        return new GeneralSwipeAction(Swipe.SLOW, GeneralLocation.TOP_RIGHT,
+                GeneralLocation.TOP_LEFT, Press.FINGER);
     }
 
     private static ViewAction swipeFromBottomToTop() {
@@ -39,17 +37,15 @@ public class MainActivityTest {
     }
 
     private static ViewAction swipeFromLeftToRight() {
-        return new GeneralSwipeAction(Swipe.SLOW, GeneralLocation.CENTER_LEFT,
-                GeneralLocation.CENTER_RIGHT, Press.FINGER);
+        return new GeneralSwipeAction(Swipe.SLOW, GeneralLocation.TOP_LEFT,
+                GeneralLocation.TOP_RIGHT, Press.FINGER);
     }
 
     @Test
-    public void testCanGreetUsers() {
-        // onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+    public void testCanSwipe() {
         onView(withId(R.id.timetableBtn)).perform(click());
         Espresso.onView(ViewMatchers.withId(R.id.recyclerView)).perform(swipeFromRightToLeft());
         Espresso.onView(ViewMatchers.withId(R.id.recyclerView)).perform(swipeFromBottomToTop());
         Espresso.onView(ViewMatchers.withId(R.id.recyclerView)).perform(swipeFromLeftToRight());
-        // onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
     }
 }
