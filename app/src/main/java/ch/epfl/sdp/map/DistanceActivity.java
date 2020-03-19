f8b05fb48daf89bc7ab20d08e516b7a9d9394c10
dmizr <david.mizrahi@epfl.ch>
2020-03-19 20:41:06 +0100

    Optimize imports

--- app/src/main/java/ch/epfl/sdp/map/DistanceActivity.java
+++ app/src/main/java/ch/epfl/sdp/map/DistanceActivity.java
@@ -9,16 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.text.DecimalFormat;
