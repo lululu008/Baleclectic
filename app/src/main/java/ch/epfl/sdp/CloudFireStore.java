8e0641d41d7539cbca77b21ec8bf23b1e2a31a24
lxy <lu852001630@163.com>
2020-03-20 11:20:53 +0800

    feat: update and delete user

--- app/src/main/java/ch/epfl/sdp/CloudFireStore.java
+++ app/src/main/java/ch/epfl/sdp/CloudFireStore.java
@@ -1,6 +1,8 @@
 package ch.epfl.sdp;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -8,20 +10,29 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import ch.epfl.sdp.bean.User;
 
 public class CloudFireStore {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public void addUser(){
-        User user = new User("John");
-        user.setEmail("123@gmail.com");
-        user.setScheduleId("a1");
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
 
-        DocumentReference newUserRef = db.collection("users").document();
+        User newUser = new User(user.getDisplayName());
+        newUser.setEmail(user.getEmail());
+        newUser.setScheduleId("a1");
 
-        newUserRef.set(user);
+        DocumentReference newUserRef = db.collection("users").document("test");
+
+        newUserRef.set(newUser);
+    }
+
+    public void updateUser(){
+        DocumentReference newUserRef = db.collection("users").document("test");
+
+        newUserRef.update("email", "123456@gamil.com");
     }
 
     public void getUser(){
-        DocumentReference newUserRef = db.collection("users").document();
+        DocumentReference newUserRef = db.collection("users").document("test");
 
         newUserRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
@@ -29,5 +40,9 @@ public class CloudFireStore {
                 User user = documentSnapshot.toObject(User.class);
             }
         });
+    }
+
+    public void deleteUser(){
+        db.collection("users").document("test").delete();
     }
 }
