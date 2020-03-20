12ac2da407dd279ad3997dd36fd074b584edb48f
lin <sining040@gmail.com>
2020-03-20 08:52:20 +0100

    try to fix non-exist user case

--- app/src/main/java/ch/epfl/sdp/LoginActivity.java
+++ app/src/main/java/ch/epfl/sdp/LoginActivity.java
@@ -2,6 +2,8 @@ package ch.epfl.sdp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,12 +15,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "BalclecticLogin";
     //FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
@@ -49,12 +56,29 @@ public class LoginActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user == null){
-                    Intent i = new Intent(getApplicationContext(), CreateUserProfile.class);
-                    startActivity(i);
-                }
-                //Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                db.collection("users").whereEqualTo("email", user.getEmail()).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                                    }
+                                } else {
+                                    //Log.d(TAG, "Error getting documents: ", task.getException());
+                                    //Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), CreateUserProfile.class));
+
+                                }
+                            }
+                        });
+                //if (user == null){
+                //    startActivity(new Intent(getApplicationContext(), CreateUserProfile.class));
+                //}else{
+                //    Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+                //    startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                //}
             } else {
                 Toast.makeText(this, ""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
             }
