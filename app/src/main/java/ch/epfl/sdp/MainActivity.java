981a4a0c4afa3bdce38b35363a50f5a377cf0b05
JiajunSang <meetjiajun@gmail.com>
2020-03-28 16:46:48 +0800

    added top tool bar, with notification and menu button, excluding left side bar

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -2,10 +2,15 @@ package ch.epfl.sdp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import ch.epfl.sdp.map.MapSelectionActivity;
 import ch.epfl.sdp.timetable.Timetable;
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void NewFunctions() {
         loginBtn();
+        toolbar();
     }
 
     private void loginBtn() {
@@ -55,4 +61,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void toolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar_mainActivity);
+        toolbar.setTitle("Home Page");
+        toolbar.setSubtitle("SubTitle");
+        //set the logo on top
+        toolbar.setLogo((R.mipmap.ic_launcher));
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_menu);
+
+        //actions of popup menu
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_notifications:
+                        Toast.makeText(MainActivity.this, "Notifications !", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Settings !", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_about:
+                        Toast.makeText(MainActivity.this, "About !", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
