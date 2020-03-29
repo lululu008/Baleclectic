28ca5b5fda68a39a441627436469828290fd4742
lxy <lu852001630@163.com>
2020-03-29 18:37:23 +0800

    fix: add lines in mock class

--- app/src/androidTest/java/ch/epfl/sdp/mockCloudStore.java
+++ app/src/androidTest/java/ch/epfl/sdp/mockCloudStore.java
@@ -5,7 +5,10 @@ import ch.epfl.sdp.dataModel.User;
 public class mockCloudStore implements CloudStoreInterface {
     @Override
     public void addNewUser(User newUser) {
-
+        int test1 = 1;
+        int test2 = 0;
+        int test3 = test1 + test2;
+        newUser.setGender(test3);
     }
 
     @Override
